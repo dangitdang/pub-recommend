c1058d6012a60410e5e6255259c10a6578a2d6aa
Dang Pham <dangpham@mit.edu>
2016-03-08 17:20:47 -0500

    Added logic to remove mutual exclusive action

--- utils/recs.js
+++ utils/recs.js
@@ -12,64 +12,59 @@ var Q = require('q');
 
 
 var eventToAction = function(event) {
-  return event.action
-}
+  return event.action;
+};
 var createMutualExs = function(obj) {
-  var dict = obj
+  var dict = obj;
   R.forEach(function(k) {
-    dict[k] = obj[k]
+    dict[k] = obj[k];
   }, R.keys(obj));
   return dict;
-}
+};
 
 var mutualExclusives = createMutualExs(config.exclusiveActions);
 
-
 var inputValidation = function(events, action) {
-  var actions = R.map(eventToAction, events)
-  var conjugate = mutualExclusives[action]
+  var actions = R.map(eventToAction, events);
+  var conjugate = mutualExclusives[action];
   if (conjugate) {
     if (R.contains(conjugate, actions)) {
-      return false
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
 var Recs = {
   inputAction: function(journal, user, pub, action) {
     var deferred = Q.defer();
     ger.namespace_exists(journal)
       .then(function(value) {
-        var now = moment();
-        now.add(10, 'y');
-        var event = {
-          namespace: journal,
-          person: user,
-          action: action,
-          thing: pub,
-          expires_at: now.format('YYYY-MM-DD')
-        };
         if (value) {
           ger.find_events(journal, {
             person: user,
             thing: pub
           }).then(function(events) {
-            if (inputValidation(events, action)) {
-              //TODO : Update action based by priority
-              console.log("Action: " + action)
-              console.log("Already contained: " + mutualExclusives[action])
-              return deferred.reject(mutualExclusives[action]);
+            if (!inputValidation(events, action)) {
+              if (config.exclusiveActions[action]) {
+                var toBeRemoved = config.exclusiveActions[action];
+                Recs.deleteAction(journal, user, pub, toBeRemoved);
+              } else {
+                return deferred.reject(mutualExclusives[action]);
+              }
             }
-            ger.events([event]).then(function() {
-              return deferred.resolve(null);
-            });
+            return Recs.insertEvent(journal, user, pub, action)
+              .then(function(){
+                deferred.resolve(null);
+              });
           });
         }
-        ger.initialize_namespace(journalId).then(function() {
-          ger.events([event]).then(function() {
-            deferred.resolve(null);
-          });
+        ger.initialize_namespace(journal).then(function() {
+          console.log('initialized namespace');
+          Recs.insertEvent(journal, user, pub, action)
+            .then(function() {
+              deferred.resolve(null);
+            });
         });
       });
     return deferred.promise;
@@ -87,19 +82,38 @@ var Recs = {
           actions: config.actions,
           filter_previous_actions: ['read', 'dislike']
         }).then(function(recommendations) {
-          //TODO: Add featured pubs and defaults recs
+          //TODO: Add featured pubs and defaults rec
           deferred.resolve(recommendations);
         });
       });
     return deferred.promise;
   },
-  deleteAction : function(journal, user, pub, action){
+  deleteAction: function(journal, user, pub, action) {
     var deferred = Q.defer();
+    console.log('deleting', user, pub, action);
     ger.delete_events(journal, user, pub, action)
-      .then(function(count){
+      .then(function(count) {
         deferred.resolve(count);
       });
     return deferred.promise;
+  },
+  insertEvent: function(journal, user, pub, action) {
+    var deferred = Q.defer();
+    var now = moment();
+    now.add(10, 'y');
+    var event = {
+      namespace: journal,
+      person: user,
+      action: action,
+      thing: pub,
+      expires_at: now.format('YYYY-MM-DD')
+    };
+    console.log('created event');
+    ger.events([event]).then(function(){
+      console.log('inserted event');
+      deferred.resolve(null);
+    });
+    return deferred.promise;
   }
-}
+};
 module.exports = Recs;
