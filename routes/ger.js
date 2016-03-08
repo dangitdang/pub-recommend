06032b1d18ee3904398aa168515feaaa2fe2359f
Dang Pham <dangpham@mit.edu>
2016-03-08 16:22:29 -0500

    Created helper utils for adding, deleting actions, and getting recommendations

--- routes/ger.js
+++ routes/ger.js
@@ -1,79 +1,41 @@
-var router = require('express').Router()
-var g = require('ger');
-var config = require('../config.js');
-var knex = require('knex')(config.db);
-var ems = new g.PsqlESM({
-    knex: knex
-});
-var ger = new g.GER(ems);
-var moment = require('moment');
+var router = require('express').Router();
+var utils = require('../utils/recs.js');
 
 /*
-	
+
 */
 router.post('/:journal', function(req, res, next) {
     var journalId = req.params.journal,
         action = req.body.action,
         pubId = req.body.pub,
         user = req.body.user;
-
-    ger.namespace_exists(journalId).then(function(value) {
-        var now = moment();
-        now.add(10, 'y');
-        var event = {
-            namespace: journalId,
-            person: user,
-            action: action,
-            thing: pubId,
-            expires_at: now.format('YYYY-MM-DD')
-        }
-        if (!value) {
-            ger.initialize_namespace(journalId)
-                .then(function() {
-                    ger.events([event]).then(function() {
-                        res.send('ok. initialized new namespace')
-                    });
-                })
-        } else {
-            ger.events([event])
-                .then(function() {
-                    res.send('ok')
-                });
-        }
-    });
+    utils.inputAction(journalId, user, pubId, action)
+        .then(function(error){
+          if (!error){
+            res.sendStatus(200);
+          }
+        }, function(containedAction){
+          res.sendStatus(200);
+        });
 });
 
 
 router.get('/:journal', function(req, res, next){
 	var journalId = req.params.journal,
 		user = req.query.user;
-
-	ger.namespace_exists(journalId).then(function(value){
-		if (!value) {
-			return res.json({
-				error : 'journalID not found'
-			})
-		} 
-		ger.recommendations_for_person(journalId, user,{
-            actions: config.actions,
-            filter_previous_actions: ['read','dislike']
-        }).then(function(recommendations){
-            res.json({
-                recommendations : recommendations
-            });
+  utils.getRecommendations(journalId, user)
+      .then(function(recs){
+        res.json({
+          recommendations : recs
         });
-	});
+      }, function(err){
+        res.json({
+          error : err
+        });
+      });
 });
 
 
 
 
-module.exports = router
-
-
-ger.find_events('movies', {
-    person: 'dangpham',
-    thing: 'titanic'
-}).then(function(events){
-    console.log(events);
-})
+module.exports = router;
--- utils/recs.js
+++ utils/recs.js
@@ -37,41 +37,69 @@ var inputValidation = function(events, action) {
 }
 
 var Recs = {
-    inputAction: function(journal, user, pub, action) {
-      var deferred = Q.defer();
-      ger.namespace_exists(journalId)
-        .then(function(value) {
-            var now = moment();
-            now.add(10, 'y');
-            var event = {
-              namespace: journalId,
-              person: user,
-              action: action,
-              thing: pubId,
-              expires_at: now.format('YYYY-MM-DD')
+  inputAction: function(journal, user, pub, action) {
+    var deferred = Q.defer();
+    ger.namespace_exists(journal)
+      .then(function(value) {
+        var now = moment();
+        now.add(10, 'y');
+        var event = {
+          namespace: journal,
+          person: user,
+          action: action,
+          thing: pub,
+          expires_at: now.format('YYYY-MM-DD')
+        };
+        if (value) {
+          ger.find_events(journal, {
+            person: user,
+            thing: pub
+          }).then(function(events) {
+            if (inputValidation(events, action)) {
+              //TODO : Update action based by priority
+              console.log("Action: " + action)
+              console.log("Already contained: " + mutualExclusives[action])
+              return deferred.reject(mutualExclusives[action]);
             }
-
-            if (value) {
-              ger.find_events(journal, {
-                person: user,
-                thing: pub
-              }).then(function(events) {
-                if (inputValidation(events, action)) {
-                  //TODO : Update action based by priority 
-                  deferred.reject(mutualExclusives[action]);
-                }
-                ger.events([event]).then(function() {
-                  deferred.resolve(null);
-                });
-              });
-            }
-            ger.initialize_namespace(journalId).then(function() {
-              ger.events([event]).then(function() {
-                deferred.resolve(null);
-              });
+            ger.events([event]).then(function() {
+              return deferred.resolve(null);
             });
-          }
+          });
+        }
+        ger.initialize_namespace(journalId).then(function() {
+          ger.events([event]).then(function() {
+            deferred.resolve(null);
+          });
+        });
       });
-      return deferred.promise();
+    return deferred.promise;
   },
+  getRecommendations: function(journal, user) {
+    var deferred = Q.defer();
+    ger.namespace_exists(journal)
+      .then(function(value) {
+        if (!value) {
+          return deferred.reject({
+            error: 'Journal not found'
+          });
+        }
+        ger.recommendations_for_person(journal, user, {
+          actions: config.actions,
+          filter_previous_actions: ['read', 'dislike']
+        }).then(function(recommendations) {
+          //TODO: Add featured pubs and defaults recs
+          deferred.resolve(recommendations);
+        });
+      });
+    return deferred.promise;
+  },
+  deleteAction : function(journal, user, pub, action){
+    var deferred = Q.defer();
+    ger.delete_events(journal, user, pub, action)
+      .then(function(count){
+        deferred.resolve(count);
+      });
+    return deferred.promise;
+  }
 }
+module.exports = Recs;
