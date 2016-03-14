dc90288ee7794aa17a254745bac7e9e22f51f756
Dang Pham <dangpham@mit.edu>
2016-03-14 16:37:12 -0400

    change db link to env variable

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports.db = {
 	client : 'pg',
-	connection: 'postgres://nrqlklez:21pj36gSxBwH5E_75qP5E1QRsv8-kaRM@pellefant.db.elephantsql.com:5432/nrqlklez',
-}
+	connection: process.env.POSTGRES,
+};
 module.exports.actions = {
 	'read' : 1,
 	'dislike' : -5,
 	'like' : 2,
 	'comment' : 1,
 	'feature' : 10
-}
+};
 module.exports.exclusiveActions = {
 	'like' : 'dislike',
-}
+};
